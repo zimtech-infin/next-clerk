bbf3c9364d54aadfe94c2d4fb08c91b1a8104ea3
Kelvin Zimmerman <kelvin@zimtechllc.us>
2024-03-14 13:23:07 -0400

    updates

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-2xl font-bold mb-5'>NEXT.JS AND CLERK, ENDLESS POSSIBILITIESS</h1>
+      <p className='mb-5'>
+        Example of next js and clerk authentication 
+      </p>
+    </>
+  );
+}
